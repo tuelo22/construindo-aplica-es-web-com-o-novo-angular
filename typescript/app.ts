a9c95ffaf6c63c49a5a54fa38a198df0d99fa1ad
tuelo22 <julio_fpsm@hotmail.com>
2019-03-26 00:09:19 -0300

    Finalizando a aula 11 seção 3

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,27 +1,39 @@
-let messege: string = "Help me,  Obi-Wan Kenobi. You're my only hope!"
-console.log(messege)
+class Spacecraft{
 
-let episode: number = 4
-console.log("This is episode " + 4)
-episode = episode + 1
-console.log("Next episode is " + episode)
+    constructor(public propulsor: string){}
 
-let favoriteDroid: string
-favoriteDroid = 'BB-8'
-console.log("My favorite droid" + favoriteDroid)
-
-let isEnoughToBeatMf = function (parsecs: number) : boolean {
-    return parsecs < 12
-}
-let distance = 11
-console.log(`Is ${distance} parsecs enought to beat Millennium Falcon ? ${isEnoughToBeatMf(distance) ? 'YES': 'NO'}`)
-
-let call = (name: string) => console.log(`Do you copy, ${name} ?`)
-call('R2')
-
-function inc (speed: number, inc: number = 1) : number{
-    return speed + inc
+    jumpIntoHyperspace(){
+        console.log(`Entring hyperspace with ${this.propulsor}`)
+    }
 }
 
-console.log(`inc (5,1) = ${inc(5,1)}`)
-console.log(`inc (5) = ${inc(5)}`)
+let ship = new Spacecraft('hyperdrive')
+
+ship.jumpIntoHyperspace()
+
+class MilleniumFalcon extends Spacecraft implements Containership{
+    cargoContainers: number;
+
+    constructor(){
+        super('hyperdrive')
+        this.cargoContainers = 2
+    }
+
+    jumpIntoHyperspace(){
+        if (Math.random() >= 0.5){
+            super.jumpIntoHyperspace()
+        }else{ console.log('Failed to jump into hyperspace')}
+    }    
+}
+
+let falcon = new MilleniumFalcon()
+
+falcon.jumpIntoHyperspace()
+
+interface Containership {
+// se colocar ? e o atributo deixa de ser obrigatório a implementação
+    cargoContainers: number
+}
+
+let goodForTheJob = (ship: Containership) => ship.cargoContainers > 2
+console.log(`Is falcon good for the job ? ${goodForTheJob (falcon) ? 'yes' : 'no'}`) 
