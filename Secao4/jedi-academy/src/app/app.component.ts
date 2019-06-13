a16ea4299a6164b02d8f08eba3ec9a3ad4a78545
tuelo22 <julio_fpsm@hotmail.com>
2019-06-12 23:31:35 -0300

    Finalizando modulo.

--- Secao4/jedi-academy/src/app/app.component.ts
+++ Secao4/jedi-academy/src/app/app.component.ts
@@ -1,11 +1,16 @@
+import { Student } from './student/student.model';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'}
-  leia = {name: 'Leia', isJedi: false}
-  han = {name: 'Han Solo', isJedi: false}
+  
+  students: Student[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Leia', isJedi: false},
+    {name: 'Han Solo', isJedi: false}
+  ]
 }
